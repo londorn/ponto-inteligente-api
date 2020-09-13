8fef76ac5535e87a93f660bd1626e2ca63dc1801
Willian <willian.449@gmail.com>
2020-09-13 14:46:56 -0300

    Correcao Junit Test Jupiter

--- src/test/java/br/com/will/pontointeligente/api/PontoInteligenteApplicationTests.java
+++ src/test/java/br/com/will/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -1,6 +1,6 @@
 package br.com.will.pontointeligente.api;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
